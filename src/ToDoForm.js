ebff89eec1f278d55e4bddbb1af50a3d204c0357
Mona Temchin <monatemchin@gmail.com>
2018-03-20 17:57:52 -0400

    Add data array, post.js
    Add array to state data to enter tasks, add post.js functional component to display them

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 class ToDoForm extends Component {
   constructor(props) {
-    super: props;
+    super(props);
     this.state = {
-      title: '';
-      description: ''
+      todos : [
+          { title: 'walk the cat',    description: 'take Yogi out for a spin around the block' },
+          { title: 'buy groceries',   description: 'Go to Fairway and get Silk coffee creamer and lemons' },
+          { title: 'clean apartment', description: 'A hopeless task! It's a mess!' },
+          { title: 'do taxes',        description: 'File for extension for 2018' },
+          { title: 'gaming',          description: "Play Pearl's Peril, June's Journey, Crosswords, and Ruzzle on iPad' }
+      ]
     }
     this.handleClick  = this.handleClick.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,16 +25,16 @@ class ToDoForm extends Component {
   }
 
   handleClick(event) {
-    this.setState({title: event.target.title});
+    this.setState({title: event.target.value});
 
   }
 
   handleDesc(event) {
-    this.setState({description: event.target.description});
+    this.setState({description: event.target.value});
 
   }
 
-  render {
+  render() {
     return (
       <form className="ToDoForm" onSubmit={this.handleSubmit.bind(this)}>
         <div>
