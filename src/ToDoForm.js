1fcb225dd6f36a803271c041896dfb90d8ccc0a9
Mona Temchin <monatemchin@gmail.com>
2018-03-25 21:13:10 -0400

    Added new stuff to render from an array
    Added new code to App, TodoForm, and Post to render from an array

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -4,13 +4,9 @@ class ToDoForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos : [
-          { title: 'walk the cat',    description: 'take Yogi out for a spin around the block' },
-          { title: 'buy groceries',   description: 'Go to Fairway and get Silk coffee creamer and lemons' },
-          { title: 'clean apartment', description: 'A hopeless task! It's a mess!' },
-          { title: 'do taxes',        description: 'File for extension for 2018' },
-          { title: 'gaming',          description: "Play Pearl's Peril, June's Journey, Crosswords, and Ruzzle on iPad' }
-      ]
+      title: '',
+      description: ''
+
     }
     this.handleClick  = this.handleClick.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,9 +14,10 @@ class ToDoForm extends Component {
   }
   handleSubmit(event) {
     alert('Submitted!');
-    this.setState({title: '',
-                     description: ''
-                   })
+    // this.setState({title: '',
+    //                description: ''
+                 // });
+
 
   }
 
